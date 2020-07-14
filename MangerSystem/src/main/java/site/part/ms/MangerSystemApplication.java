1fe7ba8b8525f1a4484372884f3bc2487710d200
zhp <love_freedomer@yeah.net>
2020-07-14 12:35:08 +0800

    最原始的刚刚创建项目的状态

---
+++ MangerSystem/src/main/java/site/part/ms/MangerSystemApplication.java
@@ -0,0 +1,13 @@
+package site.part.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangerSystemApplication.class, args);
+	}
+
+}
---
+++ MangerSystem/src/test/java/site/part/ms/MangerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package site.part.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
